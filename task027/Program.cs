662c3f07e41a7bd3fbac217221c10283bf097856
Vyacheslav <grinksic@gmail.com>
2022-08-16 01:45:28 +0300

    homework is done

---
+++ task027/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Введите число : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int n1 = N;
+int count = 0;
+for (count = 0; N > 0; count++)
+{
+    N = N / 10;
+}
+
+int length = count;
+int sum = 0;
+
+string numberText = Convert.ToString(n1);
+
+
+for (int i = 0; i <= length - 1; i++)
+{
+    sum = sum + numberText[i]-'0';
+}
+Console.WriteLine($"Сумма цифр в числе = {sum}");
---
+++ task029/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+// Реш 1. Принимаем на ввод.
+
+Console.Write("Введите элемент 1: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите элемент 2: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите элемент 3: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите элемент 4: ");
+int n4 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите элемент 5: ");
+int n5 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите элемент 6: ");
+int n6 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите элемент 7: ");
+int n7 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите элемент 8: ");
+int n8 = Convert.ToInt32(Console.ReadLine());
+
+int[] array = { n1, n2, n3, n4, n5, n6, n7, n8};
+
+Console.WriteLine(string.Join(", ", array));
+
+
+//Реш 2. Рандомно до 99
+
+// int[] arr = new int[8];
+// Random rand = new Random();
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     arr[i] = rand.Next(0, 99);
+// }
+
+Console.WriteLine(string.Join(", ", array));
